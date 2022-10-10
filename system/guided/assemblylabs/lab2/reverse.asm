635c378cf905a748c0fb788813c5481455b6b113
bazylevnik0 <bazylevnik0@gmail.com>
2022-10-10 16:14:39 +0300

    assemblylabs: lab2: 52 push segmentation fault

--- system/guided/assemblylabs/lab2/reverse.asm
+++ system/guided/assemblylabs/lab2/reverse.asm
@@ -12,10 +12,27 @@ _start:
     mov rdx, 100 ; count chars
     syscall
 
+    ;mov rax, [string]
+    ;push rax
+    ;inc rax
+    ;push rax
+    ;inc rax
+  
+    ;mov rbx, string
+    ;pop rax
+    ;mov byte[rbx + 0],al
+    ;pop rax
+    ;mov byte[rbx + 1],al
+    
     ; push to stack
-    mov rax, string
+    mov rax, [string]
+    mov r9, 0
     call _loop_in
 
+    ; pop from stack
+    ;mov rbx, string
+    ;mov r10, 0
+    ;call _loop_out
 
     ; print string
     mov rax, 1  ; sys_write
@@ -30,11 +47,21 @@ _start:
     syscall
 
 _loop_in:
-   cmp al, 0
-   je _loopquit
-   push rbx
-   inc rax
-   jmp _loop_in
+    cmp al, 0
+    je _loopquit
+    push rax
+    inc rax
+    inc r9
+    jmp _loop_in
+
+_loop_out:
+    cmp r9, 0
+    je _loopquit
+    pop rax
+    mov byte[rbx + r10],al
+    dec r9
+    inc r10
+    jmp _loop_out
 
 _loopquit:
     ret
