e6f8c57398d5826b8f1822cdc3242a81386a77d1
bazylevnik0 <bazylevnik0@gmail.com>
2022-11-22 00:08:56 +0300

    fix

--- web/free/elements/3dmenu/index.js
+++ web/free/elements/3dmenu/index.js
@@ -98,7 +98,7 @@ const scene_meal = createScene_meal();
 
 
 // Append glTF model to scene.
-BABYLON.SceneLoader.Append("./", "scene.gltf", scene_meal, function (scene) {
+BABYLON.SceneLoader.Append("https://bazylevnik0.github.io/learning/web/free/elements/3dmenu/", "scene.gltf", scene_meal, function (scene) {
       scene.createDefaultCameraOrLight(true, true, true);
       scene.activeCamera.alpha += Math.PI;
       scene.meshes[0].rotationQuaternion = null;
@@ -128,7 +128,7 @@ BABYLON.SceneLoader.Append("./", "scene.gltf", scene_meal, function (scene) {
         };
 
 	const scene_water = createScene_water();
- BABYLON.SceneLoader.Append("./", "scene.gltf", scene_water, function (scene) {
+ BABYLON.SceneLoader.Append("https://bazylevnik0.github.io/learning/web/free/elements/3dmenu/", "scene.gltf", scene_water, function (scene) {
 
       scene.createDefaultCameraOrLight(true, true, true);
         scene.activeCamera.alpha += Math.PI;
