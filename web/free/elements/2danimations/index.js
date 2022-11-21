a5b9c8bac7608055d7abdfa0d98a547523849a74
bazylevnik0 <bazylevnik0@gmail.com>
2022-11-21 23:57:34 +0300

    fix

--- web/free/elements/2danimations/index.js
+++ web/free/elements/2danimations/index.js
@@ -9,12 +9,12 @@ var cloudl2 = new Image();
 var cloudr2 = new Image();
 function init() {
   if (window.innerWidth > 2100) { 
-  	line.src    = 'https://bazylevnik0.github.io/practice/web/2danimations/main/line+.png';
-  	color.src   = 'https://bazylevnik0.github.io/practice/web/2danimations/main/color+.png';
-  	cloudl1.src = 'https://bazylevnik0.github.io/practice/web/2danimations/main/cloudl1+.png';
-  	cloudl2.src = 'https://bazylevnik0.github.io/practice/web/2danimations/main/cloudl2+.png';
-  	cloudr1.src = 'https://bazylevnik0.github.io/practice/web/2danimations/main/cloudr1+.png';
-  	cloudr2.src = 'https://bazylevnik0.github.io/practice/web/2danimations/main/cloudr2+.png';
+  	line.src    = './line+.png';
+  	color.src   = './color+.png';
+  	cloudl1.src = './cloudl1+.png';
+  	cloudl2.src = './cloudl2+.png';
+  	cloudr1.src = './cloudr1+.png';
+  	cloudr2.src = './cloudr2+.png';
   } else {
   	line.src    = 'https://bazylevnik0.github.io/practice/web/2danimations/main/line.png';
   	color.src   = 'https://bazylevnik0.github.io/practice/web/2danimations/main/color.png';
