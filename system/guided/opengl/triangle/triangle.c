eee81e8c983481ecd3224cea01fb43d9e4f40597
bazylevnik0 <bazylevnik0@gmail.com>
2023-09-04 11:13:04 +0300

    add

--- system/guided/opengl/triangle/triangle.c
+++ system/guided/opengl/triangle/triangle.c
@@ -1,16 +1,21 @@
 #include <gtk/gtk.h>
 #include <epoxy/gl.h>
 
+static GLfloat vertices[] = {
+   -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f,  1.0f, 0.0f,
+};
+unsigned int VBO, VAO;
+static GLuint programID;
+GtkWidget *gl_area;
+
 static void
 button_press(GtkEventControllerKey *controller,
      guint                           keyval,
      guint                           keycode,
      GdkModifierType                 state,
-     gpointer                        data)
-{
-  g_print("button pressed\n");
-  //if(keycode == 111)button_direction_clicked_helper("button_top",data);
-}
+     gpointer                        data);
 
 static void
 realize (GtkGLArea *area)
@@ -25,7 +30,53 @@ realize (GtkGLArea *area)
   if (gtk_gl_area_get_error (area) != NULL)
     return;
 
-  g_print("realize\n");
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);  
+  glBindVertexArray(VAO);  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glBindVertexArray(0); 
+
+  // Source of shaders
+  const char *vertex_shader_string = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+  const char *fragment_shader_string = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+   // Create the shaders
+   GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+   GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+   // Compile shaders
+   glShaderSource(VertexShaderID, 1, &vertex_shader_string , NULL);
+   glCompileShader(VertexShaderID);
+  
+   glShaderSource(FragmentShaderID, 1, &fragment_shader_string , NULL);
+   glCompileShader(FragmentShaderID);
+
+   // Create program
+   programID = glCreateProgram();
+   glAttachShader(programID, VertexShaderID);
+   glAttachShader(programID, FragmentShaderID);
+   glLinkProgram(programID);
+
+   glDeleteShader(VertexShaderID);
+   glDeleteShader(FragmentShaderID);
+
+   glUseProgram(programID);
+   glBindVertexArray(VAO); 
+
+   g_print("realize\n");
 }
 
 static void
@@ -42,8 +93,9 @@ unrealize (GtkWidget *widget)
 static gboolean
 render (GtkGLArea *area, GdkGLContext *context)
 {
-  glClearColor (0.0f, 0.0f, 1.0f, 0.5f);
-  glClear (GL_COLOR_BUFFER_BIT);
+  glClearColor (0.0f, 1.0f, 0.0f, 0.5f);
+  glClear (GL_COLOR_BUFFER_BIT );
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 
   g_print("render\n");
 
@@ -61,21 +113,16 @@ activate (GtkApplication* app,
   gtk_window_set_default_size (GTK_WINDOW (window), 600, 600);
   
   // create a GtkGLArea instance
-  GtkWidget *gl_area = gtk_gl_area_new ();
+  gl_area = gtk_gl_area_new ();
   gtk_widget_set_size_request (gl_area, 600, 600);
   gtk_window_set_child (GTK_WINDOW(window), gl_area);
   // connect to the "render" and "realize" signals
   g_signal_connect (gl_area, "realize", G_CALLBACK (realize), NULL); 
   g_signal_connect (gl_area, "unrealize", G_CALLBACK (unrealize), NULL);
   g_signal_connect (gl_area, "render" , G_CALLBACK (render) , NULL); 
-  // add input
-  GtkEventController *buttons_controller = gtk_event_controller_key_new();
-  gtk_widget_add_controller( GTK_WIDGET(window) , buttons_controller );
-  g_signal_connect( buttons_controller, "key-pressed", G_CALLBACK( button_press ), NULL );
  
   gtk_widget_set_visible (window,true);
 }
-
 
 int
 main (int    argc,
